bd9f04859e85e67bc218e345396dee349f95eea4
Vladislav Yarmak <vladislav@vm-0.com>
2018-11-28 16:01:21 +0200

    remove retired fields

--- tests/policy_test.py
+++ tests/policy_test.py
@@ -23,21 +23,11 @@ class TestPolicyList(unittest.TestCase):
     def test_links_ok(self):
         policy_list = self.policy["policies"]
         policy_aliases = self.policy["policy-aliases"]
-        pinsets = self.policy["pinsets"]
-
-        def _validate_actual_policy(pol):
-            if "pin" in pol:
-                self.assertIn(pol["pin"], pinsets)
 
         for pol_key in policy_list:
             pol = policy_list[pol_key]
             if "policy-alias" in pol:
                 self.assertIn(pol["policy-alias"], policy_aliases)
-            else:
-                _validate_actual_policy(pol)
-
-        for pol in policy_aliases:
-            _validate_actual_policy(pol)
 
 
 if __name__ == '__main__':
